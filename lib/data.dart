0f077aac53b89a8e641b1b166e0d62dee73edc50
Rashaud Savage <rsavage8@gatech.edu>
2019-11-20 19:43:54 -0800

    Initial commit

---
+++ lib/data.dart
@@ -0,0 +1,17 @@
+List <String> images = [
+
+  "assets/image_04.jpg",
+  "assets/image_03.jpg",
+  "assets/image_02.jpg",
+  "assets/image_01.png",
+
+];
+
+
+
+List <String> titles = [
+  "Haunted Ground",
+  "Fallen In Love",
+  "The Dreaming Moon",
+  "1001 Arabian Nights",
+];
